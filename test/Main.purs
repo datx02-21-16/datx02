b5292e337f81158713a4e40f6b483e894aa5c20b
Axel Forsman <axelsfor@gmail.com>
2021-02-16 13:28:14 +0100

    Use purescript-spec for unit testing

--- test/Main.purs
+++ test/Main.purs
@@ -3,9 +3,10 @@ module Test.Main where
 import Prelude
 
 import Effect (Effect)
-import Effect.Class.Console (log)
+import Effect.Aff (launchAff_)
+import Test.Spec.Discovery (discover)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (runSpec)
 
 main :: Effect Unit
-main = do
-  log "🍝"
-  log "You should add some tests."
+main = launchAff_ $ discover """Test\..*""" >>= runSpec [consoleReporter]
