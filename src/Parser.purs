9acb0cd3b9ce6244586e6c99930ff631645249eb
Axel Forsman <axelsfor@gmail.com>
2021-03-02 15:51:45 +0100

    Add formula parser (#11)
    * Add parser for formulas
    
    * Add infix equality predicate
    
    * Add substitution data type
    
    * Add unification algorithm
    
    * Fix parsing of double negation
    
    Workaround the fact that Text.Parsing.Parser.Expr.buildExprParser does
    not allow prefix operators of the same precedence to occur more than
    once by parsing all consecutive "¬" signs en masse.

---
+++ src/Parser.purs
@@ -0,0 +1,98 @@
+module Parser (formula, parseFormula) where
+
+import Prelude
+
+import Control.Alternative ((<|>))
+import Control.Lazy (fix)
+import Data.Array as Array
+import Data.Char.Unicode (isLower)
+import Data.Either (Either)
+import Data.Identity (Identity)
+
+import Text.Parsing.Parser (Parser, ParserT, ParseError, runParser)
+import Text.Parsing.Parser.Combinators (option, chainl1, lookAhead, (<?>))
+import Text.Parsing.Parser.String (oneOf, satisfy, eof)
+import Text.Parsing.Parser.Token (GenLanguageDef(..), TokenParser, makeTokenParser, upper, letter)
+import Text.Parsing.Parser.Expr (OperatorTable, Assoc(..), Operator(..), buildExprParser)
+
+import Formula (Variable(..), Term(..), Formula(..))
+
+token :: TokenParser
+token = makeTokenParser languageDef
+  where
+    languageDef = LanguageDef
+                  { commentStart: ""
+                  , commentEnd: ""
+                  , commentLine: ""
+                  , nestedComments: false
+                  , identStart: letter
+                  , identLetter: letter
+                  , opStart: oneOf ['¬', '∧', '∨', '→', '∀', '∃']
+                  , opLetter: oneOf []
+                  , reservedNames: []
+                  , reservedOpNames: ["¬", "∧", "∨", "→", "∀", "∃"]
+                  , caseSensitive: true
+                  }
+
+-- | Parse a lowercase letter.
+lower :: forall m. Monad m => ParserT String m Char
+lower = satisfy isLower <?> "lowercase letter"
+
+variableSymbol :: Parser String String
+variableSymbol = lookAhead lower *> token.identifier
+variable :: Parser String Variable
+variable = Variable <$> variableSymbol
+
+argumentList :: forall a. Parser String a -> Parser String (Array a)
+argumentList p = Array.fromFoldable <$> token.parens (token.commaSep p)
+
+term :: Parser String Term
+term = do
+      symbol <- variableSymbol
+      -- Constants require empty argument list ("c()") to disambiguate from variables
+      option (Var $ Variable symbol) (App symbol <$> argumentList term)
+
+-- | Parse a single predicate variable such as P(x).
+predicate :: Parser String Formula
+predicate = let
+  predicateSymbol = token.symbol "=" <|> lookAhead upper *> token.identifier
+  -- The equality predicate is usually written using infix notation
+  equality = do
+    x <- term
+    _ <- token.symbol "="
+    y <- term
+    pure $ Predicate "=" [x, y]
+  in do
+    symbol <- predicateSymbol
+    -- For nullary predicates the argument list is optional
+    args <- option [] (argumentList term)
+    pure $ Predicate symbol args
+  <|> equality
+
+-- | Parse a single formula.
+formula :: Parser String Formula
+formula = fix allFormulas
+  where
+    forallParser = Forall <$> (token.reservedOp "∀" *> variable)
+    existsParser = Exists <$> (token.reservedOp "∃" *> variable)
+
+    -- Required because buildExprParser does not allow multiple prefix
+    -- operators of the same precedence (e.g. ¬¬A).
+    chained p = chainl1 p $ pure (<<<)
+
+    opTable :: OperatorTable Identity String Formula
+    opTable =
+      [ [ Prefix $ chained (token.reservedOp "¬" $> Not)
+        , Prefix forallParser
+        , Prefix existsParser ]
+      , [ Infix (token.reservedOp "∧" $> And) AssocLeft
+        , Infix (token.reservedOp "∨" $> Or) AssocLeft
+        , Infix (token.reservedOp "→" $> Implies) AssocRight ] ]
+
+    allFormulas p = let
+      singleFormula = token.parens p <|> predicate
+      in token.whiteSpace *> buildExprParser opTable singleFormula
+
+-- | Returns the result of parsing a formula from the specified string.
+parseFormula :: String -> Either ParseError Formula
+parseFormula = flip runParser $ formula <* eof
---
+++ test/Parser.purs
@@ -0,0 +1,33 @@
+module Test.Parser where
+
+import Prelude
+import Data.Either (Either(..))
+import Test.Spec (Spec, describe, it)
+import Test.Spec.Assertions (shouldEqual)
+
+import Formula (Variable(..), Term(..), Formula(..))
+import Parser (parseFormula)
+
+spec :: Spec Unit
+spec = describe "Formula parser" do
+  it "should work" do
+    parseFormula "¬(A ∧ B)" `shouldEqual`
+      Right (Not (And
+                  (Predicate "A" [])
+                  (Predicate "B" [])))
+  it "should handle whitespace" do
+    parseFormula " ∃ x P ( x ) → ( A ) " `shouldEqual`
+      Right (Implies
+             (Exists (Variable "x") (Predicate "P" [(Var $ Variable "x")]))
+             (Predicate "A" []))
+  it "parses constants and variables" do
+    parseFormula "∀x Le(x, c())" `shouldEqual`
+      Right (Forall (Variable "x")
+            (Predicate "Le" [Var $ Variable "x", App "c" []]))
+  it "parses the equality predicate" do
+    parseFormula "=(x, y)" `shouldEqual`
+      Right (Predicate "=" [Var $ Variable "x", Var $ Variable "y"])
+    parseFormula "x = y" `shouldEqual`
+      Right (Predicate "=" [Var $ Variable "x", Var $ Variable "y"])
+  it "should parse double negation" do
+    parseFormula "¬¬P" `shouldEqual` Right (Not (Not (Predicate "P" [])))
