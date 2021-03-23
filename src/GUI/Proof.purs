4890846a755ec808bf5202025c99a1f059f0a230
Axel Forsman <axelsfor@gmail.com>
2021-03-23 11:05:58 +0100

    Add some comments to GUI Proof module

--- src/GUI/Proof.purs
+++ src/GUI/Proof.purs
@@ -23,8 +23,11 @@ import Halogen.HTML.Properties as HP
 
 import GUI.SymbolInput as SI
 import GUI.SymbolInput (symbolInput)
---import GUI.Panels as P
 import GUI.Rules as R
+
+-- For GUI proof state we use a representation that is easy to modify,
+-- i.e. has a single contiguous array of all rows. When rendering or
+-- validating we map this to a tree with subproof nodes.
 
 data Rule = Rule String
           | Assumption { boxEndIdx :: Int }
@@ -37,20 +40,21 @@ ruleText :: Rule -> String
 ruleText (Rule s) = s
 ruleText (Assumption _) = "Ass."
 
-type Row
+type ProofRow
   = { formulaText :: String
     , rule :: Rule
     , ruleArgs :: Array String
     }
 
-emptyRow :: Row
+-- | A newly added row.
+emptyRow :: ProofRow
 emptyRow = { formulaText: "", rule: Rule "", ruleArgs: [] }
 
 -- | Only stores endpoints of boxes since assumptions naturally define start points.
 type State
   = { premises :: String
     , conclusion :: String
-    , rows :: Array Row
+    , rows :: Array ProofRow
     , boxEnds :: Set Int
     }
 
@@ -75,99 +79,100 @@ proof =
                                    , handleQuery = handleQuery
                                    }
     }
+
+initialState _ = { premises: ""
+                 , conclusion: ""
+                 , rows: [ emptyRow ] }
+
+handleQuery :: forall a state action output m. MonadEffect m => Query a -> H.HalogenM state action Slots output m (Maybe a)
+handleQuery (Tell command a) = case command of
+  R.AndElim1 -> do
+    H.liftEffect $ logShow "and elim 1"
+    -- When we are here the button click from AndElim1 has been propagated all
+    -- the way to the proof component, and we can now update the state accordingly,
+    -- inserting new rows etc.
+    pure Nothing
+  R.AndElim2 -> do
+    H.liftEffect $ logShow "and elim 2"
+    pure Nothing
+  R.AndIntro -> do
+    H.liftEffect $ logShow "and introduction"
+    pure Nothing
+  _ -> pure Nothing
+
+render st =
+    HH.div
+        [ HP.classes [ HH.ClassName "proof-rows" ] ]
+        (NonEmpty.head $ foldlWithIndex
+           (\i (currentBox@{ elems }:|parentBoxes) proofRow
+            -> let closeBoxesIfPossible = case _ of
+                     {endIdx}:|_ | endIdx < i -> unsafeCrashWith "Unreachable (box ends outside of parent)"
+
+                     {elems: currentElems, endIdx}:|parent:rest
+                     | endIdx == i -> closeBoxesIfPossible $ parent
+                                      { elems = Array.snoc parent.elems
+                                                $ HH.div [ HP.classes [ HH.ClassName "proof-box" ] ] currentElems }:|rest
+                     x -> x
+              in closeBoxesIfPossible case proofRow.rule of
+                     Rule s -> (currentBox { elems = Array.snoc elems $ row i proofRow }):|parentBoxes
+                     Assumption { boxEndIdx }
+                       -> { elems: [row i proofRow], endIdx: boxEndIdx }
+                          :|currentBox:parentBoxes
+           )
+           ({ elems: [], endIdx: Array.length st.rows }:|Nil)
+           st.rows).elems
+
   where
-  initialState _ = { premises: ""
-                   , conclusion: ""
-                   , rows: [ {formulaText: "", rule: Assumption {boxEndIdx: 1}, ruleArgs: [] }, emptyRow, {formulaText: "", rule: Assumption {boxEndIdx: 2}, ruleArgs: [] } ] }
+    row :: Int -> ProofRow -> HH.HTML _ _
+    row i { formulaText, rule }
+      = HH.div
+        [ HP.classes [ HH.ClassName "columns", HH.ClassName "is-mobile", HH.ClassName "proof-row" ] ]
+        ( [ HH.div
+              [ HP.classes [ HH.ClassName "column", HH.ClassName "is-narrow" ] ]
+              [ HH.h4
+                  [ HP.classes [ HH.ClassName "title", HH.ClassName "row-index" ] ]
+                  [ HH.text (show (1+i)) ]
+              ]
+          , HH.div
+              [ HP.classes [ HH.ClassName "column", HH.ClassName "formula-field" ] ]
+              [ HH.slot _symbolInput (2*i) (symbolInput "Enter formula") formulaText $ case _ of
+                   SI.NewValue s -> UpdateFormula i s
+                   SI.EnterPressed -> NewRowBelow i
+              ]
+          , HH.div
+              [ HP.classes [ HH.ClassName "column", HH.ClassName "is-narrow" ] ]
+              [ HH.span
+                [ HP.classes [ HH.ClassName "rule-field" ] ]
+                [ HH.slot _symbolInput (2*i+1) (symbolInput "Rule") (ruleText rule) $ case _ of
+                   SI.NewValue s -> UpdateRule i s
+                   SI.EnterPressed -> NewRowBelow i ]
+              ]
+          ]
+        )
 
-  handleQuery :: forall a state action. Query a -> H.HalogenM state action Slots output m (Maybe a)
-  handleQuery (Tell command a) = case command of
-    R.AndElim1 -> do
-      H.liftEffect $ logShow "and elim 1"
-      -- When we are here the button click from AndElim1 has been propagated all
-      -- the way to the proof component, and we can now update the state accordingly,
-      -- inserting new rows etc.
-      pure Nothing
-    R.AndElim2 -> do
-      H.liftEffect $ logShow "and elim 2"
-      pure Nothing
-    R.AndIntro -> do
-      H.liftEffect $ logShow "and introduction"
-      pure Nothing
-    _ -> pure Nothing
+handleAction = case _ of
+  UpdateFormula i s ->
+    H.modify_
+       \st -> st { rows = unsafePartial $ fromJust $ Array.modifyAt i _ { formulaText = s } st.rows }
+  UpdateRule i s ->
+    H.modify_ \st -> st { rows = unsafePartial $ fromJust
+                       $ Array.modifyAt i _ { rule = ruleFromString s i }
+                       st.rows }
+  NewRowBelow i -> do
+    H.modify_
+      \st -> let
+      incrBoxEnds = mapWithIndex \j -> case _ of
+        row@{ rule: Assumption { boxEndIdx } } | i <= boxEndIdx
+          -> row { rule = Assumption { boxEndIdx: boxEndIdx + 1 } }
+        x -> x
+      in st { rows = unsafePartial $ fromJust $ Array.insertAt (i+1) emptyRow
+                $ incrBoxEnds st.rows }
+    -- Focus the newly added row
+    H.tell _symbolInput (2*(i+1)) SI.Focus
 
-  render st =
-      HH.div
-          [ HP.classes [ HH.ClassName "proof-rows" ] ]
-          (NonEmpty.head $ foldlWithIndex
-             (\i (currentBox@{ elems }:|parentBoxes) proofRow
-              -> let closeBoxesIfPossible = case _ of
-                       {endIdx}:|_ | endIdx < i -> unsafeCrashWith "Unreachable (box ends outside of parent)"
+  _ -> unsafeCrashWith "unimpl"
 
-                       {elems: currentElems, endIdx}:|parent:rest
-                       | endIdx == i -> closeBoxesIfPossible $ parent
-                                        { elems = Array.snoc parent.elems
-                                                  $ HH.div [ HP.classes [ HH.ClassName "proof-box" ] ] currentElems }:|rest
-                       x -> x
-                in closeBoxesIfPossible case proofRow.rule of
-                       Rule s -> (currentBox { elems = Array.snoc elems $ row i proofRow }):|parentBoxes
-                       Assumption { boxEndIdx }
-                         -> { elems: [row i proofRow], endIdx: boxEndIdx }
-                            :|currentBox:parentBoxes
-             )
-             ({ elems: [], endIdx: Array.length st.rows }:|Nil)
-             st.rows).elems
-
-  row :: Int -> Row -> HH.HTML _ _
-  row i { formulaText, rule }
-    = HH.div
-      [ HP.classes [ HH.ClassName "columns", HH.ClassName "is-mobile", HH.ClassName "proof-row" ] ]
-      ( [ HH.div
-            [ HP.classes [ HH.ClassName "column", HH.ClassName "is-narrow" ] ]
-            [ HH.h4
-                [ HP.classes [ HH.ClassName "title", HH.ClassName "row-index" ] ]
-                [ HH.text (show (1+i)) ]
-            ]
-        , HH.div
-            [ HP.classes [ HH.ClassName "column", HH.ClassName "formula-field" ] ]
-            [ HH.slot _symbolInput (2*i) (symbolInput "Enter formula") formulaText $ case _ of
-                 SI.NewValue s -> UpdateFormula i s
-                 SI.EnterPressed -> NewRowBelow i
-            ]
-        , HH.div
-            [ HP.classes [ HH.ClassName "column", HH.ClassName "is-narrow" ] ]
-            [ HH.span
-              [ HP.classes [ HH.ClassName "rule-field" ] ]
-              [ HH.slot _symbolInput (2*i+1) (symbolInput "Rule") (ruleText rule) $ case _ of
-                 SI.NewValue s -> UpdateRule i s
-                 SI.EnterPressed -> NewRowBelow i ]
-            ]
-        ]
-      )
-
-  handleAction = case _ of
-    UpdateFormula i s ->
-      H.modify_
-         \st -> st { rows = unsafePartial $ fromJust $ Array.modifyAt i _ { formulaText = s } st.rows }
-    UpdateRule i s ->
-      H.modify_ \st -> st { rows = unsafePartial $ fromJust
-                         $ Array.modifyAt i _ { rule = ruleFromString s i }
-                         st.rows }
-    NewRowBelow i -> do
-      H.modify_
-        \st -> let
-        incrBoxEnds = mapWithIndex \j -> case _ of
-          row@{ rule: Assumption { boxEndIdx } } | i <= boxEndIdx
-            -> row { rule = Assumption { boxEndIdx: boxEndIdx + 1 } }
-          x -> x
-        in st { rows = unsafePartial $ fromJust $ Array.insertAt (i+1) emptyRow
-                  $ incrBoxEnds st.rows }
-      -- Focus the newly added row
-      H.tell _symbolInput (2*(i+1)) SI.Focus
-
-    _ -> unsafeCrashWith "unimpl"
-
-  ruleFromString :: String -> Int -> Rule
-  ruleFromString s rowIdx
-    | s == "Ass." || s == "as" = Assumption { boxEndIdx: rowIdx }
-    | otherwise = Rule s
+ruleFromString :: String -> Int -> Rule
+ruleFromString s rowIdx
+  | s == "Ass." || s == "as" = Assumption { boxEndIdx: rowIdx }
+  | otherwise = Rule s
