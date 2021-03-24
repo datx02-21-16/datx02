e53320194b3ca2f92869def794d3f41d99c78cc9
Freddy Abrahamsson <freddya@student.chalmers.se>
2021-03-24 16:25:04 +0100

    First box escape
    Simple box escape. Cannot handle double box endings.

--- src/GUI/Proof.purs
+++ src/GUI/Proof.purs
@@ -11,26 +11,21 @@ import Effect.Console (logShow)
 import Data.Set as Set
 import Data.Set (Set)
 import Data.FoldableWithIndex (foldlWithIndex)
-
 import Data.List as List
 import Data.List (List(Nil), (:))
 import Data.NonEmpty as NonEmpty
 import Data.NonEmpty (NonEmpty, (:|))
-
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Properties as HP
-
 import GUI.SymbolInput as SI
 import GUI.SymbolInput (symbolInput)
+--import GUI.Panels as P
 import GUI.Rules as R
 
--- For GUI proof state we use a representation that is easy to modify,
--- i.e. has a single contiguous array of all rows. When rendering or
--- validating we map this to a tree with subproof nodes.
-
-data Rule = Rule String
-          | Assumption { boxEndIdx :: Int }
+data Rule
+  = Rule String
+  | Assumption { boxEndIdx :: Int }
 
 instance showRule :: Show Rule where
   show (Rule s) = s
@@ -38,23 +33,23 @@ instance showRule :: Show Rule where
 
 ruleText :: Rule -> String
 ruleText (Rule s) = s
+
 ruleText (Assumption _) = "Ass."
 
-type ProofRow
+type Row
   = { formulaText :: String
     , rule :: Rule
     , ruleArgs :: Array String
     }
 
--- | A newly added row.
-emptyRow :: ProofRow
+emptyRow :: Row
 emptyRow = { formulaText: "", rule: Rule "", ruleArgs: [] }
 
 -- | Only stores endpoints of boxes since assumptions naturally define start points.
 type State
   = { premises :: String
     , conclusion :: String
-    , rows :: Array ProofRow
+    , rows :: Array Row
     , boxEnds :: Set Int
     }
 
@@ -62,117 +57,171 @@ data Action
   = UpdateFormula Int String
   | UpdateRule Int String
   | NewRowBelow Int
+  | TryExit Int
 
 _symbolInput = Proxy :: Proxy "symbolInput"
 
-data Query a = Tell R.Rules a
+data Query a
+  = Tell R.Rules a
 
-type Slots = ( proof :: forall output. H.Slot Query output Int
-             , symbolInput :: H.Slot SI.Query SI.Output Int)
+type Slots
+  = ( proof :: forall output. H.Slot Query output Int
+    , symbolInput :: H.Slot SI.Query SI.Output Int
+    )
 
 proof :: forall input output m. MonadEffect m => H.Component Query input output m
 proof =
   H.mkComponent
     { initialState
     , render
-    , eval: H.mkEval H.defaultEval { handleAction = handleAction
-                                   , handleQuery = handleQuery
-                                   }
+    , eval:
+        H.mkEval
+          H.defaultEval
+            { handleAction = handleAction
+            , handleQuery = handleQuery
+            }
+    }
+  where
+  initialState _ =
+    { premises: ""
+    , conclusion: ""
+    , rows: [ emptyRow ]
     }
 
-initialState _ = { premises: ""
-                 , conclusion: ""
-                 , rows: [ emptyRow ] }
+  handleQuery :: forall a state action. Query a -> H.HalogenM state action Slots output m (Maybe a)
+  handleQuery (Tell command a) = case command of
+    R.AndElim1 -> do
+      H.liftEffect $ logShow "and elim 1"
+      -- When we are here the button click from AndElim1 has been propagated all
+      -- the way to the proof component, and we can now update the state accordingly,
+      -- inserting new rows etc.
+      pure Nothing
+    R.AndElim2 -> do
+      H.liftEffect $ logShow "and elim 2"
+      pure Nothing
+    R.AndIntro -> do
+      H.liftEffect $ logShow "and introduction"
+      pure Nothing
+    _ -> pure Nothing
 
-handleQuery :: forall a state action output m. MonadEffect m => Query a -> H.HalogenM state action Slots output m (Maybe a)
-handleQuery (Tell command a) = case command of
-  R.AndElim1 -> do
-    H.liftEffect $ logShow "and elim 1"
-    -- When we are here the button click from AndElim1 has been propagated all
-    -- the way to the proof component, and we can now update the state accordingly,
-    -- inserting new rows etc.
-    pure Nothing
-  R.AndElim2 -> do
-    H.liftEffect $ logShow "and elim 2"
-    pure Nothing
-  R.AndIntro -> do
-    H.liftEffect $ logShow "and introduction"
-    pure Nothing
-  _ -> pure Nothing
-
-render st =
+  render st =
     HH.div
-        [ HP.classes [ HH.ClassName "proof-rows" ] ]
-        (NonEmpty.head $ foldlWithIndex
-           (\i (currentBox@{ elems }:|parentBoxes) proofRow
-            -> let closeBoxesIfPossible = case _ of
-                     {endIdx}:|_ | endIdx < i -> unsafeCrashWith "Unreachable (box ends outside of parent)"
-
-                     {elems: currentElems, endIdx}:|parent:rest
-                     | endIdx == i -> closeBoxesIfPossible $ parent
-                                      { elems = Array.snoc parent.elems
-                                                $ HH.div [ HP.classes [ HH.ClassName "proof-box" ] ] currentElems }:|rest
-                     x -> x
-              in closeBoxesIfPossible case proofRow.rule of
-                     Rule s -> (currentBox { elems = Array.snoc elems $ row i proofRow }):|parentBoxes
-                     Assumption { boxEndIdx }
-                       -> { elems: [row i proofRow], endIdx: boxEndIdx }
-                          :|currentBox:parentBoxes
-           )
-           ({ elems: [], endIdx: Array.length st.rows }:|Nil)
-           st.rows).elems
-
-  where
-    row :: Int -> ProofRow -> HH.HTML _ _
-    row i { formulaText, rule }
-      = HH.div
-        [ HP.classes [ HH.ClassName "columns", HH.ClassName "is-mobile", HH.ClassName "proof-row" ] ]
-        ( [ HH.div
-              [ HP.classes [ HH.ClassName "column", HH.ClassName "is-narrow" ] ]
-              [ HH.h4
-                  [ HP.classes [ HH.ClassName "title", HH.ClassName "row-index" ] ]
-                  [ HH.text (show (1+i)) ]
-              ]
-          , HH.div
-              [ HP.classes [ HH.ClassName "column", HH.ClassName "formula-field" ] ]
-              [ HH.slot _symbolInput (2*i) (symbolInput "Enter formula") formulaText $ case _ of
-                   SI.NewValue s -> UpdateFormula i s
-                   SI.EnterPressed -> NewRowBelow i
-              ]
-          , HH.div
-              [ HP.classes [ HH.ClassName "column", HH.ClassName "is-narrow" ] ]
-              [ HH.span
-                [ HP.classes [ HH.ClassName "rule-field" ] ]
-                [ HH.slot _symbolInput (2*i+1) (symbolInput "Rule") (ruleText rule) $ case _ of
-                   SI.NewValue s -> UpdateRule i s
-                   SI.EnterPressed -> NewRowBelow i ]
-              ]
-          ]
+      [ HP.classes [ HH.ClassName "proof-rows" ] ]
+      ( NonEmpty.head
+            $ foldlWithIndex
+                ( \i (currentBox@{ elems } :| parentBoxes) proofRow ->
+                    let
+                      closeBoxesIfPossible = case _ of
+                        { endIdx } :| _
+                          | endIdx < i -> unsafeCrashWith "Unreachable (box ends outside of parent)"
+                        { elems: currentElems, endIdx } :| parent : rest
+                          | endIdx == i ->
+                            closeBoxesIfPossible
+                              $ parent
+                                  { elems =
+                                    Array.snoc parent.elems
+                                      $ HH.div [ HP.classes [ HH.ClassName "proof-box" ] ] currentElems
+                                  }
+                              :| rest
+                        x -> x
+                    in
+                      closeBoxesIfPossible case proofRow.rule of
+                        Rule s -> (currentBox { elems = Array.snoc elems $ row i proofRow }) :| parentBoxes
+                        Assumption { boxEndIdx } ->
+                          { elems: [ row i proofRow ], endIdx: boxEndIdx }
+                            :| currentBox
+                            : parentBoxes
+                )
+                ({ elems: [], endIdx: Array.length st.rows } :| Nil)
+                st.rows
         )
+        .elems
 
-handleAction = case _ of
-  UpdateFormula i s ->
-    H.modify_
-       \st -> st { rows = unsafePartial $ fromJust $ Array.modifyAt i _ { formulaText = s } st.rows }
-  UpdateRule i s ->
-    H.modify_ \st -> st { rows = unsafePartial $ fromJust
-                       $ Array.modifyAt i _ { rule = ruleFromString s i }
-                       st.rows }
-  NewRowBelow i -> do
-    H.modify_
-      \st -> let
-      incrBoxEnds = mapWithIndex \j -> case _ of
-        row@{ rule: Assumption { boxEndIdx } } | i <= boxEndIdx
-          -> row { rule = Assumption { boxEndIdx: boxEndIdx + 1 } }
+  row :: Int -> Row -> HH.HTML _ _
+  row i { formulaText, rule } =
+    HH.div
+      [ HP.classes [ HH.ClassName "columns", HH.ClassName "is-mobile", HH.ClassName "proof-row" ] ]
+      ( [ HH.div
+            [ HP.classes [ HH.ClassName "column", HH.ClassName "is-narrow" ] ]
+            [ HH.h4
+                [ HP.classes [ HH.ClassName "title", HH.ClassName "row-index" ] ]
+                [ HH.text (show (1 + i)) ]
+            ]
+        , HH.div
+            [ HP.classes [ HH.ClassName "column", HH.ClassName "formula-field" ] ]
+            [ HH.slot _symbolInput (2 * i) (symbolInput "Enter formula") formulaText
+                $ case _ of
+                    SI.NewValue s -> UpdateFormula i s
+                    SI.EnterPressed -> NewRowBelow i
+                    SI.ShiftEnterPressed -> TryExit i
+            ]
+        , HH.div
+            [ HP.classes [ HH.ClassName "column", HH.ClassName "is-narrow" ] ]
+            [ HH.span
+                [ HP.classes [ HH.ClassName "rule-field" ] ]
+                [ HH.slot _symbolInput (2 * i + 1) (symbolInput "Rule") (ruleText rule)
+                    $ case _ of
+                        SI.NewValue s -> UpdateRule i s
+                        SI.EnterPressed -> NewRowBelow i
+                        SI.ShiftEnterPressed -> TryExit i
+                ]
+            ]
+        ]
+      )
+
+  handleAction = case _ of
+    UpdateFormula i s ->
+      H.modify_ \st ->
+        st
+          { rows =
+            unsafePartial
+              $ fromJust
+              $ Array.modifyAt i _ { formulaText = s } st.rows
+          }
+    UpdateRule i s ->
+      H.modify_ \st ->
+        st
+          { rows =
+            unsafePartial $ fromJust
+              $ Array.modifyAt i _ { rule = ruleFromString s i }
+                  st.rows
+          }
+    NewRowBelow i -> do
+      H.modify_ (createBelow i)
+      -- Focus the newly added row
+      H.tell _symbolInput (2 * (i + 1)) SI.Focus
+    TryExit i -> do
+      H.modify_ (exitBox i)
+      H.tell _symbolInput (2 * (i + 1)) SI.Focus
+    _ -> unsafeCrashWith "unimpl"
+    where
+    createBelow i st = st { rows = unsafePartial $ fromJust $ Array.insertAt (i + 1) emptyRow $ (incrBoxEnds i) st.rows }
+
+    exitBox i st = st { rows = unsafePartial $ fromJust $ Array.insertAt (i + 1) emptyRow $ (incrBoxEnds (i + 1)) st.rows }
+
+    incrBoxEnds i =
+      mapWithIndex \j -> case _ of
+        row@{ rule: Assumption { boxEndIdx } }
+          | i <= boxEndIdx -> row { rule = Assumption { boxEndIdx: boxEndIdx + 1 } }
         x -> x
-      in st { rows = unsafePartial $ fromJust $ Array.insertAt (i+1) emptyRow
-                $ incrBoxEnds st.rows }
-    -- Focus the newly added row
-    H.tell _symbolInput (2*(i+1)) SI.Focus
 
-  _ -> unsafeCrashWith "unimpl"
+  getAllEndings :: State -> Array Int
+  getAllEndings st = map rToE $ boxes st
+    where
+    rToE :: Row -> Int
+    rToE r = case r.rule of
+      Assumption { boxEndIdx } -> boxEndIdx
+      _ -> unsafeCrashWith "Row is not a box."
 
-ruleFromString :: String -> Int -> Rule
-ruleFromString s rowIdx
-  | s == "Ass." || s == "as" = Assumption { boxEndIdx: rowIdx }
-  | otherwise = Rule s
+    boxes :: State -> Array Row
+    boxes st = Array.filter isBox st.rows
+
+    isBox :: Row -> Boolean
+    isBox r = case r.rule of
+      Assumption _ -> true
+      _ -> false
+
+  ruleFromString :: String -> Int -> Rule
+  ruleFromString s rowIdx
+    | s == "Ass." || s == "as" = Assumption { boxEndIdx: rowIdx }
+    | otherwise = Rule s
--- src/GUI/SymbolInput.purs
+++ src/GUI/SymbolInput.purs
@@ -6,7 +6,6 @@ import Data.Maybe (Maybe(..))
 import Partial.Unsafe (unsafeCrashWith)
 import Data.String as String
 import Effect.Class (class MonadEffect)
-
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
@@ -15,32 +14,37 @@ import Web.HTML.HTMLElement as HTMLElement
 import Web.HTML.HTMLInputElement as HTMLInputElement
 import Web.UIEvent.KeyboardEvent as KeyboardEvent
 import Web.UIEvent.KeyboardEvent (KeyboardEvent)
-
 import Data.String.Regex (replace)
 import Data.Tuple (Tuple(..))
-
 import GUI.Utils (makeRegex)
 
 substitute :: String -> String
-substitute = foldl (<<<) identity
-             $ (\(Tuple from to) -> replace (makeRegex from) to) <$> ss
+substitute =
+  foldl (<<<) identity
+    $ (\(Tuple from to) -> replace (makeRegex from) to)
+    <$> ss
   where
-    ss = [ Tuple "an" "∧"
-         , Tuple "or" "∨"
-         , Tuple "->" "→"
-         , Tuple "no" "¬"
-           -- Quantifiers
-         , Tuple "f[ao]" "∀"
-         , Tuple "ex|te" "∃"
-         , Tuple "bo" "⊥"
-         ]
+  ss =
+    [ Tuple "an" "∧"
+    , Tuple "or" "∨"
+    , Tuple "->" "→"
+    , Tuple "no" "¬"
+    -- Quantifiers
+    , Tuple "f[ao]" "∀"
+    , Tuple "ex|te" "∃"
+    , Tuple "bo" "⊥"
+    ]
 
-data Query a = Focus a
+data Query a
+  = Focus a
 
-type Input = String
+type Input
+  = String
 
-data Output = NewValue String
-            | EnterPressed
+data Output
+  = NewValue String
+  | EnterPressed
+  | ShiftEnterPressed
 
 data Action
   = OnInput String
@@ -52,60 +56,72 @@ ref = H.RefLabel "inputRef"
 
 -- | Text field component with abbreviations for logic symbols.
 symbolInput :: forall m. MonadEffect m => String -> H.Component Query Input Output m
-symbolInput placeholder
-  = H.mkComponent
+symbolInput placeholder =
+  H.mkComponent
     { initialState
-      , render
-      , eval: H.mkEval H.defaultEval { handleAction = handleAction
-                                     , receive = Just <<< Receive
-                                     , handleQuery = handleQuery }}
+    , render
+    , eval:
+        H.mkEval
+          H.defaultEval
+            { handleAction = handleAction
+            , receive = Just <<< Receive
+            , handleQuery = handleQuery
+            }
+    }
   where
-    initialState s = { s, cursor: String.length s }
-    render st
-      = HH.input
-        [ HP.value st.s
-        , HP.placeholder placeholder
-        , HP.classes [ HH.ClassName "input", HH.ClassName "is-primary" ]
-        , HP.type_ HP.InputText
-        , HP.ref ref
-        , HE.onValueInput OnInput
-        , HE.onKeyDown KeyDown
-        ]
-    setStr s' = do
-        { s, cursor } <- H.get
-        -- Note: Assumes all edits happen before cursor
-        let cursor' = cursor + String.length s' - String.length s
-        -- Update the value: This will prompt a redraw
-        H.modify_ _ { s = s', cursor = cursor' }
+  initialState s = { s, cursor: String.length s }
 
-        -- Reset the cursor position
-        el <- H.getHTMLElementRef ref
-        case el >>= HTMLInputElement.fromHTMLElement of
-          Just x -> H.liftEffect do
-            HTMLInputElement.setSelectionStart cursor' x
-            HTMLInputElement.setSelectionEnd cursor' x
-          _ -> unsafeCrashWith "not yet rendered"
+  render st =
+    HH.input
+      [ HP.value st.s
+      , HP.placeholder placeholder
+      , HP.classes [ HH.ClassName "input", HH.ClassName "is-primary" ]
+      , HP.type_ HP.InputText
+      , HP.ref ref
+      , HE.onValueInput OnInput
+      , HE.onKeyDown KeyDown
+      ]
 
-    handleAction = case _ of
-      OnInput s -> do
-        el <- H.getHTMLElementRef ref
-        case el >>= HTMLInputElement.fromHTMLElement of
-          Just x -> do
-            selectionStart <- H.liftEffect $ HTMLInputElement.selectionStart x
-            H.modify_ _ { s = s, cursor = selectionStart }
-          _ -> unsafeCrashWith "not yet rendered"
+  setStr s' = do
+    { s, cursor } <- H.get
+    -- Note: Assumes all edits happen before cursor
+    let
+      cursor' = cursor + String.length s' - String.length s
+    -- Update the value: This will prompt a redraw
+    H.modify_ _ { s = s', cursor = cursor' }
+    -- Reset the cursor position
+    el <- H.getHTMLElementRef ref
+    case el >>= HTMLInputElement.fromHTMLElement of
+      Just x ->
+        H.liftEffect do
+          HTMLInputElement.setSelectionStart cursor' x
+          HTMLInputElement.setSelectionEnd cursor' x
+      _ -> unsafeCrashWith "not yet rendered"
 
-        let s' = substitute s
-        H.raise $ NewValue s' -- Output the new value
-      Receive s' -> setStr s'
-      KeyDown ev -> when (KeyboardEvent.key ev == "Enter")
-                    $ H.raise EnterPressed
+  handleAction = case _ of
+    OnInput s -> do
+      el <- H.getHTMLElementRef ref
+      case el >>= HTMLInputElement.fromHTMLElement of
+        Just x -> do
+          selectionStart <- H.liftEffect $ HTMLInputElement.selectionStart x
+          H.modify_ _ { s = s, cursor = selectionStart }
+        _ -> unsafeCrashWith "not yet rendered"
+      let
+        s' = substitute s
+      H.raise $ NewValue s' -- Output the new value
+    Receive s' -> setStr s'
+    KeyDown ev ->
+      when (KeyboardEvent.key ev == "Enter")
+        if KeyboardEvent.shiftKey ev then
+          H.raise ShiftEnterPressed
+        else
+          H.raise EnterPressed
 
-    handleQuery :: forall action a. Query a -> H.HalogenM _ action () _ m (Maybe a)
-    handleQuery = case _ of
-      Focus a -> do
-        el <- H.getHTMLElementRef ref
-        case el >>= HTMLInputElement.fromHTMLElement of
-          Just x -> H.liftEffect $ HTMLElement.focus (HTMLInputElement.toHTMLElement x)
-          _ -> unsafeCrashWith "not yet rendered"
-        pure $ Just a
+  handleQuery :: forall action a. Query a -> H.HalogenM _ action () _ m (Maybe a)
+  handleQuery = case _ of
+    Focus a -> do
+      el <- H.getHTMLElementRef ref
+      case el >>= HTMLInputElement.fromHTMLElement of
+        Just x -> H.liftEffect $ HTMLElement.focus (HTMLInputElement.toHTMLElement x)
+        _ -> unsafeCrashWith "not yet rendered"
+      pure $ Just a
