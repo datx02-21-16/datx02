89a3994446ac2a82355054a4fff06dc47187fb20
Freddy Abrahamsson <freddya@student.chalmers.se>
2021-03-30 11:06:56 +0200

    Fixed merge conflict.

--- src/GUI/Proof.purs
+++ src/GUI/Proof.purs
@@ -1,22 +1,21 @@
-module GUI.Proof where
+module GUI.Proof (Query(..), proof) where
 
 import Prelude
 import Type.Proxy (Proxy(..))
-import Data.Maybe (Maybe(..), fromJust)
+import Data.Maybe (Maybe(..), fromJust, maybe)
 import Partial.Unsafe (unsafePartial, unsafeCrashWith)
 import Data.Array as Array
 import Data.Array ((!!))
 import Data.FunctorWithIndex (mapWithIndex)
 import Effect.Class (class MonadEffect)
 import Effect.Console (logShow)
-import Data.Set as Set
-import Data.Set (Set)
 import Data.FoldableWithIndex (foldlWithIndex)
 import Data.List (List(Nil), (:))
 import Data.NonEmpty as NonEmpty
 import Data.NonEmpty ((:|))
 import Data.MediaType (MediaType(MediaType))
 import Data.Int as Int
+import Data.Tuple (Tuple(..), fst)
 import Data.String.Common (joinWith)
 import Halogen as H
 import Halogen.HTML as HH
@@ -47,6 +46,7 @@ derive instance eqRule :: Eq Rule
 
 instance showRule :: Show Rule where
   show (Rule s) = s
+  show Premise = "Premise"
   show (Assumption { boxEndIdx }) = "Assumption (box ends at " <> show boxEndIdx <> ")"
 
 ruleText :: Rule -> String
@@ -56,13 +56,14 @@ ruleText Premise = "Premise"
 
 ruleText (Assumption _) = "Ass."
 
-type Row
+type ProofRow
   = { formulaText :: String
     , rule :: Rule
     , ruleArgs :: Array String
     }
 
-emptyRow :: Row
+-- | A newly added row.
+emptyRow :: ProofRow
 emptyRow = { formulaText: "", rule: Rule "", ruleArgs: [] }
 
 -- | Only stores endpoints of boxes since assumptions naturally define start points.
@@ -106,31 +107,49 @@ proof =
             , handleQuery = handleQuery
             }
     }
+
+initialState :: forall input. input -> State
+initialState _ =
+  { conclusion: ""
+  , rows: [ emptyRow ]
+  , draggingOver: Nothing
+  }
+
+handleQuery :: forall a state action output m. MonadEffect m => Query a -> H.HalogenM state action Slots output m (Maybe a)
+handleQuery (Tell command a) = case command of
+  R.AndElim1 -> do
+    H.liftEffect $ logShow "and elim 1"
+    -- When we are here the button click from AndElim1 has been propagated all
+    -- the way to the proof component, and we can now update the state accordingly,
+    -- inserting new rows etc.
+    pure Nothing
+  R.AndElim2 -> do
+    H.liftEffect $ logShow "and elim 2"
+    pure Nothing
+  R.AndIntro -> do
+    H.liftEffect $ logShow "and introduction"
+    pure Nothing
+  _ -> pure Nothing
+
+render :: forall m. MonadEffect m => State -> H.ComponentHTML Action Slots m
+render st =
+  HH.div [ HP.classes [ HH.ClassName "proof" ] ]
+    [ proofHeader, proofRows ]
   where
-  initialState :: forall m. m -> State
-  initialState _ =
-    { premises: ""
-    , conclusion: ""
-    , rows: [ emptyRow ]
-    }
+  proofHeader =
+    HH.div
+      [ HP.classes [ HH.ClassName "proof-header" ] ]
+      [ HH.span [] [ HH.text premises, HH.text " ⊢ " ]
+      , HH.slot _symbolInput (-1) (symbolInput "Conclusion") st.conclusion UpdateConclusion
+      ]
 
-  handleQuery :: forall a state action. Query a -> H.HalogenM state action Slots output m (Maybe a)
-  handleQuery (Tell command a) = case command of
-    R.AndElim1 -> do
-      H.liftEffect $ logShow "and elim 1"
-      -- When we are here the button click from AndElim1 has been propagated all
-      -- the way to the proof component, and we can now update the state accordingly,
-      -- inserting new rows etc.
-      pure Nothing
-    R.AndElim2 -> do
-      H.liftEffect $ logShow "and elim 2"
-      pure Nothing
-    R.AndIntro -> do
-      H.liftEffect $ logShow "and introduction"
-      pure Nothing
-    _ -> pure Nothing
+  premises =
+    joinWith ", "
+      $ _.formulaText
+      <$> Array.takeWhile ((_ == Premise) <<< _.rule) st.rows
 
-  render st =
+  proofRows :: HH.HTML _ _
+  proofRows =
     HH.div
       [ HP.classes [ HH.ClassName "proof-rows" ] ]
       ( NonEmpty.head
@@ -146,36 +165,46 @@ proof =
                               $ parent
                                   { elems =
                                     Array.snoc parent.elems
-                                      $ HH.div
-                                          [ HP.classes [ HH.ClassName "proof-box" ] ]
-                                          currentElems
+                                      $ HH.div [ HP.classes [ HH.ClassName "proof-box" ] ] currentElems
                                   }
                               :| rest
                         x -> x
                     in
                       closeBoxesIfPossible case proofRow.rule of
-                        Rule s ->
-                          ( currentBox
-                              { elems =
-                                Array.snoc elems
-                                  $ row i proofRow
-                              }
-                          )
-                            :| parentBoxes
                         Assumption { boxEndIdx } ->
                           { elems: [ row i proofRow ], endIdx: boxEndIdx }
                             :| currentBox
                             : parentBoxes
+                        _ -> (currentBox { elems = Array.snoc elems $ row i proofRow }) :| parentBoxes
                 )
                 ({ elems: [], endIdx: Array.length st.rows } :| Nil)
                 st.rows
         )
         .elems
 
-  row :: Int -> Row -> HH.HTML _ _
+  row :: Int -> ProofRow -> HH.HTML _ _
   row i { formulaText, rule } =
     HH.div
-      [ HP.classes [ HH.ClassName "columns", HH.ClassName "is-mobile", HH.ClassName "proof-row" ] ]
+      [ HP.classes
+          ( [ HH.ClassName "columns", HH.ClassName "is-mobile", HH.ClassName "proof-row" ]
+              <> maybe []
+                  ( \j ->
+                      if i == j then
+                        [ HH.ClassName "dragged-over" ]
+                      else
+                        []
+                  )
+                  st.draggingOver
+          )
+      , HP.draggable true
+      , HE.onKeyDown $ RowKeyEvent i
+      , HE.onDragStart $ DragStart i
+      , HE.onDragOver $ DragOver i
+      , HE.onDragEnter $ DragEnter i
+      , HE.onDragLeave $ DragLeave i
+      , HE.onDrop $ Drop i
+      , HE.onDragEnd $ DragEnd i
+      ]
       ( [ HH.div
             [ HP.classes [ HH.ClassName "column", HH.ClassName "is-narrow" ] ]
             [ HH.h4
@@ -184,67 +213,92 @@ proof =
             ]
         , HH.div
             [ HP.classes [ HH.ClassName "column", HH.ClassName "formula-field" ] ]
-            [ HH.slot _symbolInput (2 * i) (symbolInput "Enter formula") formulaText
-                $ case _ of
-                    SI.NewValue s -> UpdateFormula i s
-                    SI.EnterPressed -> NewRowBelow i
-                    SI.ShiftEnterPressed -> TryExit i
-            ]
+            [ HH.slot _symbolInput (2 * i) (symbolInput "Enter formula") formulaText (UpdateFormula i) ]
         , HH.div
             [ HP.classes [ HH.ClassName "column", HH.ClassName "is-narrow" ] ]
             [ HH.span
                 [ HP.classes [ HH.ClassName "rule-field" ] ]
-                [ HH.slot _symbolInput (2 * i + 1) (symbolInput "Rule") (ruleText rule)
-                    $ case _ of
-                        SI.NewValue s -> UpdateRule i s
-                        SI.EnterPressed -> NewRowBelow i
-                        SI.ShiftEnterPressed -> TryExit i
-                ]
+                [ HH.slot _symbolInput (2 * i + 1) (symbolInput "Rule") (ruleText rule) (UpdateRule i) ]
             ]
         ]
       )
 
-  handleAction = case _ of
-    UpdateFormula i s ->
-      H.modify_ \st ->
-        st
-          { rows =
-            unsafePartial
-              $ fromJust
-              $ Array.modifyAt i _ { formulaText = s } st.rows
-          }
-    UpdateRule i s ->
-      H.modify_ \st ->
-        st
-          { rows =
-            unsafePartial $ fromJust
-              $ Array.modifyAt i _ { rule = ruleFromString s i }
-                  st.rows
-          }
-    NewRowBelow i -> do
-      H.modify_ (createBelow i)
-      -- Focus the newly added row
-      H.tell _symbolInput (2 * (i + 1)) SI.Focus
-    TryExit i -> do
-      H.modify_ (exitBox i)
-      H.tell _symbolInput (2 * (i + 1)) SI.Focus
-    _ -> unsafeCrashWith "unimpl"
-    where
-    -- | Creates a new row directly below the given index.
-    createBelow :: Int -> State -> State
-    createBelow i st =
+-- | The media type for the index of a proof row as a string.
+rowMediaType :: MediaType
+rowMediaType = MediaType "application/x.row"
+
+handleAction :: forall output m. MonadEffect m => Action -> H.HalogenM State Action Slots output m Unit
+handleAction = case _ of
+  UpdateFormula i s -> H.modify_ \st -> st { rows = unsafePartial $ fromJust $ Array.modifyAt i _ { formulaText = s } st.rows }
+  UpdateRule i s ->
+    H.modify_ \st ->
       st
         { rows =
-          unsafePartial
-            $ fromJust
-            $ Array.insertAt (i + 1) emptyRow
-            $ (incrBoxEnds i) st.rows
+          unsafePartial $ fromJust
+            $ Array.modifyAt i _ { rule = ruleFromString s i }
+                st.rows
         }
+  RowKeyEvent i ev -> case KeyboardEvent.key ev of
+    "Enter" -> if KeyboardEvent.shiftKey ev then exitBox i else addRowBelow i
+    _ -> pure unit
+  DragStart i ev -> do
+    H.liftEffect $ DataTransfer.setData rowMediaType (show i)
+      $ DragEvent.dataTransfer ev
+  DragOver i ev -> do
+    validDropZone <- isValidDropZone i ev
+    when validDropZone do
+      H.modify_ \st -> st { draggingOver = Just i }
+      H.liftEffect $ Event.preventDefault $ DragEvent.toEvent ev
+  DragEnter i ev -> do
+    validDropZone <- isValidDropZone i ev
+    when validDropZone do
+      H.liftEffect $ DataTransfer.setDropEffect DataTransfer.Move $ DragEvent.dataTransfer ev
+      H.modify_ \st -> st { draggingOver = Just i }
+  DragLeave i ev -> do
+    draggingOver <- H.gets _.draggingOver
+    when (draggingOver /= Just i) do
+      H.liftEffect $ DataTransfer.setDropEffect DataTransfer.None $ DragEvent.dataTransfer ev
+      H.modify_ \st -> st { draggingOver = Nothing }
+  DragEnd i ev -> H.modify_ \st -> st { draggingOver = Nothing }
+  Drop i ev -> do
+    H.liftEffect $ Event.preventDefault $ DragEvent.toEvent ev
+    H.modify_ \st -> st { draggingOver = Nothing }
+    { start, end } <- draggedRows ev
+    H.modify_ \st ->
+      let
+        target = i + 1
 
-    -- | Creates a new row directly below the current index. If the current
-    --   index is at the end of a box, the new row is created outside the box.
-    exitBox :: Int -> State -> State
-    exitBox i st =
+        newStart = target - if start < target then end - start else 0
+
+        updateBoxes =
+          mapWithIndex \j -> case _ of
+            row@{ rule: Assumption { boxEndIdx } }
+              -- Moved box
+              | start <= j, j < end -> row { rule = Assumption { boxEndIdx: boxEndIdx + (newStart - start) } }
+              -- Move before/inside box
+              | i <= boxEndIdx, boxEndIdx < start -> row { rule = Assumption { boxEndIdx: boxEndIdx + (end - start) } }
+              -- Move after box
+              | start <= boxEndIdx, boxEndIdx < i -> row { rule = Assumption { boxEndIdx: boxEndIdx - (end - start) } }
+            x -> x
+
+        rows' = moveWithin target start end $ updateBoxes st.rows
+      in
+        st { rows = rows' }
+  UpdateConclusion s -> H.modify_ \st -> st { conclusion = s }
+  where
+  addRowBelow i = do
+    H.modify_ \st ->
+      st
+        { rows =
+          unsafePartial $ fromJust $ Array.insertAt (i + 1) emptyRow
+            $ incrBoxEnds i st.rows
+        }
+    H.tell _symbolInput (2 * (i + 1)) SI.Focus -- Focus the newly added row
+
+  -- | Creates a new row directly below the current index. If the current
+  --   index is at the end of a box, the new row is created outside the box.
+  exitBox i = do
+    H.modify_ \st ->
       st
         { rows =
           unsafePartial $ fromJust $ Array.insertAt (i + 1) emptyRow
@@ -255,86 +309,105 @@ proof =
               )
                 st.rows
         }
+    H.tell _symbolInput (2 * (i + 1)) SI.Focus -- Focus the newly added row
 
-    -- | Takes an index and an array of rows. Increases the ending position of
-    --   all boxes which end after the given index by one.
-    incrBoxEnds :: Int -> Array Row -> Array Row
-    incrBoxEnds i =
-      map \r -> case r of
-        row@{ rule: Assumption { boxEndIdx } }
-          | i <= boxEndIdx -> row { rule = Assumption { boxEndIdx: boxEndIdx + 1 } }
-        x -> x
+  -- | Takes an index and an array of rows. Increases the ending position of
+  --   all boxes which end after the given index by one.
+  incrBoxEnds :: Int -> Array ProofRow -> Array ProofRow
+  incrBoxEnds i =
+    map \r -> case r of
+      row@{ rule: Assumption { boxEndIdx } }
+        | i <= boxEndIdx -> row { rule = Assumption { boxEndIdx: boxEndIdx + 1 } }
+      x -> x
 
-    -- | Takes an index and an array of rows. Increases the ending position of
-    --   all boxes which end after the given index by one, except any box which
-    --   starts at the exception position.
-    incrBoxEndsWithEx :: Int -> Int -> Array Row -> Array Row
-    incrBoxEndsWithEx i e rs =
-      map
-        ( \(Tuple idx r) ->
-            if idx == e then
-              r
-            else case r of
-              row@{ rule: Assumption { boxEndIdx } }
-                | (i <= boxEndIdx) -> row { rule = Assumption { boxEndIdx: boxEndIdx + 1 } }
-              x -> x
-        )
-        (indexRows rs)
+  -- | Takes an index and an array of rows. Increases the ending position of
+  --   all boxes which end after the given index by one, except any box which
+  --   starts at the exception position.
+  incrBoxEndsWithEx :: Int -> Int -> Array ProofRow -> Array ProofRow
+  incrBoxEndsWithEx i e rs =
+    map
+      ( \(Tuple idx r) ->
+          if idx == e then
+            r
+          else case r of
+            row@{ rule: Assumption { boxEndIdx } }
+              | (i <= boxEndIdx) -> row { rule = Assumption { boxEndIdx: boxEndIdx + 1 } }
+            x -> x
+      )
+      (indexRows rs)
 
-  -- | Takes an index and a state and returns the starting position of the
-  --   innermost box which contains the index.
-  scopeStart :: Int -> State -> Int
-  scopeStart i st = fst $ innermostScope i st
-    where
-    -- | Gets the innermost box in a state which contains the given index.
-    innermostScope :: Int -> State -> Tuple Int Int
-    innermostScope i st = unsafePartial $ fromJust $ Array.last $ inBoxes i (allBoxLimits st)
+  -- | Inclusive-exclusive interval of the rows that are currently being dragged.
+  draggedRows :: DragEvent -> H.HalogenM _ _ _ _ _ { start :: Int, end :: Int }
+  draggedRows ev = do
+    start <-
+      (\s -> unsafePartial $ fromJust $ Int.fromString s)
+        <$> (H.liftEffect $ DataTransfer.getData rowMediaType $ DragEvent.dataTransfer ev)
+    rows <- H.gets _.rows
+    let
+      startRow = unsafePartial $ fromJust $ rows !! start
+    let
+      end = case startRow.rule of
+        Assumption { boxEndIdx } -> boxEndIdx + 1
+        _ -> start + 1
+    pure { start, end }
 
-    -- | Gets a list of all the boxes which a given position is in.
-    inBoxes :: Int -> Array (Tuple Int Int) -> Array (Tuple Int Int)
-    inBoxes i bs = Array.filter (inBox i) bs
+  isValidDropZone i ev = (\{ start, end } -> not (start <= i && i < end)) <$> draggedRows ev
 
-    -- | Checks if a position is inside a box.
-    inBox :: Int -> Tuple Int Int -> Boolean
-    inBox i (Tuple lo hi) = i >= lo && i <= hi
+-- | Takes an index and a state and returns the starting position of the
+--   innermost box which contains the index.
+scopeStart :: Int -> State -> Int
+scopeStart i st = fst $ innermostScope i st
+  where
+  -- | Gets the innermost box in a state which contains the given index.
+  innermostScope :: Int -> State -> Tuple Int Int
+  innermostScope i st = unsafePartial $ fromJust $ Array.last $ inBoxes i (allBoxLimits st)
 
-    -- | Extracts all boxes along with their index from a list of indexed rows.
-    indexedBoxes :: State -> Array (Tuple Int Row)
-    indexedBoxes st = Array.filter (\(Tuple i r) -> isBox r) (indexRows st.rows)
+  -- | Gets a list of all the boxes which a given position is in.
+  inBoxes :: Int -> Array (Tuple Int Int) -> Array (Tuple Int Int)
+  inBoxes i bs = Array.filter (inBox i) bs
 
-    -- | Takes a row, assumed to be the start of a box, along with it's
-    --   position in the proof. Returns a tuple with the limits of the box.
-    boxLimits :: Tuple Int Row -> Tuple Int Int
-    boxLimits (Tuple i r) = case r.rule of
-      (Assumption { boxEndIdx }) -> Tuple i boxEndIdx
-      _ -> unsafeCrashWith "Not a box."
+  -- | Checks if a position is inside a box.
+  inBox :: Int -> Tuple Int Int -> Boolean
+  inBox i (Tuple lo hi) = i >= lo && i <= hi
 
-    -- | Get a list of all boxes in a state as tuples.
-    allBoxLimits :: State -> Array (Tuple Int Int)
-    allBoxLimits st = map boxLimits $ indexedBoxes st
+  -- | Extracts all boxes along with their index from a list of indexed rows.
+  indexedBoxes :: State -> Array (Tuple Int ProofRow)
+  indexedBoxes st = Array.filter (\(Tuple i r) -> isBox r) (indexRows st.rows)
 
-  -- | Tags each line in an array with it's index in the array.
-  indexRows :: forall t. Array t -> Array (Tuple Int t)
-  indexRows arr = Array.zip (0 Array... (Array.length arr)) arr
+  -- | Takes a row, assumed to be the start of a box, along with it's
+  --   position in the proof. Returns a tuple with the limits of the box.
+  boxLimits :: Tuple Int ProofRow -> Tuple Int Int
+  boxLimits (Tuple i r) = case r.rule of
+    (Assumption { boxEndIdx }) -> Tuple i boxEndIdx
+    _ -> unsafeCrashWith "Not a box."
 
-  getAllEndings :: State -> Array Int
-  getAllEndings st = map rToE $ boxes st
-    where
-    rToE :: Row -> Int
-    rToE r = case r.rule of
-      Assumption { boxEndIdx } -> boxEndIdx
-      _ -> unsafeCrashWith "Row is not a box."
+  -- | Get a list of all boxes in a state as tuples.
+  allBoxLimits :: State -> Array (Tuple Int Int)
+  allBoxLimits st = map boxLimits $ indexedBoxes st
 
-  boxes :: State -> Array Row
-  boxes st = Array.filter isBox st.rows
+-- | Tags each line in an array with it's index in the array.
+indexRows :: forall t. Array t -> Array (Tuple Int t)
+indexRows arr = Array.zip (0 Array... (Array.length arr)) arr
 
-  -- | Check if a row is the start of a box.
-  isBox :: Row -> Boolean
-  isBox r = case r.rule of
-    Assumption _ -> true
-    _ -> false
+getAllEndings :: State -> Array Int
+getAllEndings st = map rToE $ boxes st
+  where
+  rToE :: ProofRow -> Int
+  rToE r = case r.rule of
+    Assumption { boxEndIdx } -> boxEndIdx
+    _ -> unsafeCrashWith "Row is not a box."
 
-  ruleFromString :: String -> Int -> Rule
-  ruleFromString s rowIdx
-    | s == "Ass." || s == "as" = Assumption { boxEndIdx: rowIdx }
-    | otherwise = Rule s
+boxes :: State -> Array ProofRow
+boxes st = Array.filter isBox st.rows
+
+-- | Check if a row is the start of a box.
+isBox :: ProofRow -> Boolean
+isBox r = case r.rule of
+  Assumption _ -> true
+  _ -> false
+
+ruleFromString :: String -> Int -> Rule
+ruleFromString s rowIdx
+  | s == "Ass." || s == "as" = Assumption { boxEndIdx: rowIdx }
+  | s == "pr" || s == "Premise" = Premise
+  | otherwise = Rule s
