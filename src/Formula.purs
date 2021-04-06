221bf7c6dc46c010a6f033889902196e75f699c2
Axel Forsman <axelsfor@gmail.com>
2021-04-06 12:14:14 +0200

    Fix bottom parsing (#26)
    * Format Formula/Parser modules
    
    * Fix parsing of bottom symbol

--- src/Formula.purs
+++ src/Formula.purs
@@ -1,15 +1,17 @@
-module Formula ( Variable(..)
-               , Term(..)
-               , Formula(..)
-               , Substitution
-               , singleSub
-               , class Substitutable
-               , substitute
-               , disagreementSet
-               , unify
-               , containsTerm
-               , formulaUnifier
-               ) where
+module Formula
+  ( Variable(..)
+  , Term(..)
+  , Formula(..)
+  , bottomProp
+  , Substitution
+  , singleSub
+  , class Substitutable
+  , substitute
+  , disagreementSet
+  , unify
+  , containsTerm
+  , formulaUnifier
+  ) where
 
 import Prelude
 import Data.List as List
@@ -23,17 +25,17 @@ import Data.Map as Map
 import Data.Map (Map)
 import Data.Set as Set
 import Data.Set (Set)
-
 import Data.Foldable (any, or)
-
-
 import Data.Unfoldable as Unfoldable
 
 -- | A variable symbol.
-newtype Variable = Variable String
+newtype Variable
+  = Variable String
 
 derive newtype instance eqVariable :: Eq Variable
+
 derive newtype instance ordVariable :: Ord Variable
+
 instance showVariable :: Show Variable where
   show (Variable s) = s
 
@@ -47,6 +49,7 @@ data Term
   | App String (Array Term)
 
 derive instance eqTerm :: Eq Term
+
 derive instance ordTerm :: Ord Term
 
 instance showTerm :: Show Term where
@@ -64,50 +67,80 @@ data Formula
   | Exists Variable Formula
 
 derive instance eqFormula :: Eq Formula
+
 derive instance ordFormula :: Ord Formula
 
 -- | Shows formulas using least amount of parentheses wrt. precedence.
 instance showFormula :: Show Formula where
   show = showPrec 1
     where
-      parens s = "(" <> s <> ")"
-      optParens b s = if b then parens s else s
-      showPrec _ (Predicate p []) = p
-      showPrec _ (Predicate p args) = p <> parens (joinWith ", " (show <$> args))
+    parens s = "(" <> s <> ")"
 
-      showPrec _ (Not f) = "¬" <> showPrec 4 f
-      showPrec _ (Forall x f) = "∀" <> show x <> " " <> showPrec 4 f
-      showPrec _ (Exists x f) = "∃" <> show x <> " " <> showPrec 4 f
+    optParens b s = if b then parens s else s
 
-      showPrec n (And a b) = optParens (n>3)
-                             $ showPrec 3 a <> " ∧ " <> showPrec 4 b
-      showPrec n (Or a b) = optParens (n>2)
-                             $ showPrec 2 a <> " ∨ " <> showPrec 3 b
-      showPrec n (Implies a b) = optParens (n>1)
-                             $ showPrec 2 a <> " → " <> showPrec 1 b
+    showPrec _ (Predicate p []) = p
 
-bottom :: Formula
-bottom = Predicate "⊥" []
+    showPrec _ (Predicate p args) = p <> parens (joinWith ", " (show <$> args))
+
+    showPrec _ (Not f) = "¬" <> showPrec 4 f
+
+    showPrec _ (Forall x f) = "∀" <> show x <> " " <> showPrec 4 f
+
+    showPrec _ (Exists x f) = "∃" <> show x <> " " <> showPrec 4 f
+
+    showPrec n (And a b) =
+      optParens (n > 3)
+        $ showPrec 3 a
+        <> " ∧ "
+        <> showPrec 4 b
+
+    showPrec n (Or a b) =
+      optParens (n > 2)
+        $ showPrec 2 a
+        <> " ∨ "
+        <> showPrec 3 b
+
+    showPrec n (Implies a b) =
+      optParens (n > 1)
+        $ showPrec 2 a
+        <> " → "
+        <> showPrec 1 b
+
+-- | Dedicated symbol for a proposition that is assigned a false truth value.
+bottomProp :: Formula
+bottomProp = Predicate "⊥" []
 
 -- | A substitution {t₁/v₁, ..., tₙ/vₙ}.
 -- |
 -- | All v:s must be mutually unique.
-data Substitution = Substitution (Map Variable Term)
+data Substitution
+  = Substitution (Map Variable Term)
 
 derive instance eqSubstitution :: Eq Substitution
 
 instance showSubstitution :: Show Substitution where
-  show (Substitution ss) = let
-    elems = Map.toUnfoldableUnordered ss :: Array _
-    showElem (Tuple t v) = show t <> "/" <> show v
-    in "{" <> (joinWith ", " $ showElem <$> elems) <> "}"
+  show (Substitution ss) =
+    let
+      elems = Map.toUnfoldableUnordered ss :: Array _
+
+      showElem (Tuple t v) = show t <> "/" <> show v
+    in
+      "{" <> (joinWith ", " $ showElem <$> elems) <> "}"
 
 instance semigroupSubstitution :: Semigroup Substitution where
   -- | The composition of the two substitutions.
-  append (Substitution a) λ@(Substitution b) = Substitution
-    $ Map.mapMaybeWithKey (\x t -> let t' = substitute λ t
-                                   in if t' == Var x then Nothing else Just t') a
-    `Map.union` b
+  append (Substitution a) λ@(Substitution b) =
+    Substitution
+      $ Map.mapMaybeWithKey
+          ( \x t ->
+              let
+                t' = substitute λ t
+              in
+                if t' == Var x then Nothing else Just t'
+          )
+          a
+          `Map.union`
+            b
 
 instance monoidSubstitution :: Monoid Substitution where
   mempty = Substitution Map.empty
@@ -116,11 +149,15 @@ instance monoidSubstitution :: Monoid Substitution where
 -- |
 -- | The variable v may not occur in the replacement term t.
 singleSub :: Variable -> Term -> Maybe Substitution
-singleSub v t = if v `occursIn` t then Nothing
-                else Just $ Substitution (Map.singleton v t)
-  where occursIn v1 = case _ of
-          Var v2 -> v1 == v2
-          App _ args -> any (v1 `occursIn` _) args
+singleSub v t =
+  if v `occursIn` t then
+    Nothing
+  else
+    Just $ Substitution (Map.singleton v t)
+  where
+  occursIn v1 = case _ of
+    Var v2 -> v1 == v2
+    App _ args -> any (v1 `occursIn` _) args
 
 -- | Expressions on which substitutions can be done.
 class Substitutable a where
@@ -145,8 +182,10 @@ instance substitutableFormula :: Substitutable Formula where
     Implies a b -> Implies (sub a) (sub b)
     Forall x a -> Forall x (subWithout x a)
     Exists x a -> Exists x (subWithout x a)
-    where sub = substitute θ
-          subWithout x = substitute $ Substitution (Map.delete x s)
+    where
+    sub = substitute θ
+
+    subWithout x = substitute $ Substitution (Map.delete x s)
 
 -- | The disagreement set of the specified set of expression lists.
 -- |
@@ -155,15 +194,17 @@ instance substitutableFormula :: Substitutable Formula where
 disagreementSet :: List (List Term) -> List Term
 disagreementSet xs = fromMaybe Nil $ List.find (not <<< null) $ disagreement <$> transpose xs
   where
-    -- | Returns subexpressions of a term if it shares top-most structure with head.
-    subexprs head = case head, _ of
-      Var v1, Var v2 | v1 == v2 -> Just Nil
-      App f1 args1, App f2 args2
-        | f1 == f2, Array.length args1 == Array.length args2 -> Just $ List.fromFoldable args2
-      _, _ -> Nothing
-    disagreement terms = case List.head terms of
-      Nothing -> Nil
-      Just head -> maybe terms disagreementSet (sequence $ subexprs head <$> terms)
+  -- | Returns subexpressions of a term if it shares top-most structure with head.
+  subexprs head = case head, _ of
+    Var v1, Var v2
+      | v1 == v2 -> Just Nil
+    App f1 args1, App f2 args2
+      | f1 == f2, Array.length args1 == Array.length args2 -> Just $ List.fromFoldable args2
+    _, _ -> Nothing
+
+  disagreement terms = case List.head terms of
+    Nothing -> Nil
+    Just head -> maybe terms disagreementSet (sequence $ subexprs head <$> terms)
 
 -- | Tries to find a most general unifier for the set of term lists.
 -- |
@@ -174,45 +215,54 @@ disagreementSet xs = fromMaybe Nil $ List.find (not <<< null) $ disagreement <$>
 unify :: Set (List Term) -> Maybe (Tuple Substitution (List Term))
 unify = go mempty
   where
-    -- If w is singleton σ is most general unifier
-    go σ w | Set.size w <= 1 = (Tuple σ) <$> (Set.toUnfoldable w)
-    go σ w = let
+  -- If w is singleton σ is most general unifier
+  go σ w
+    | Set.size w <= 1 = (Tuple σ) <$> (Set.toUnfoldable w)
+
+  go σ w =
+    let
       ds = disagreementSet $ List.fromFoldable w
-      sub = List.head do
-        -- Find v and t in ds such that v is a variable not occuring in t
-        v <- ds >>= case _ of
-          Var v -> pure v
-          _ -> Nil
-        t <- ds
-        Unfoldable.fromMaybe $ singleSub v t
-      in sub >>= (\λ -> go (σ <> λ) (Set.map (map (substitute λ)) w))
+
+      sub =
+        List.head do
+          -- Find v and t in ds such that v is a variable not occuring in t
+          v <-
+            ds
+              >>= case _ of
+                  Var v -> pure v
+                  _ -> Nil
+          t <- ds
+          Unfoldable.fromMaybe $ singleSub v t
+    in
+      sub >>= (\λ -> go (σ <> λ) (Set.map (map (substitute λ)) w))
 
 containsTerm :: Formula -> Term -> Boolean
 containsTerm f t = case f of
   Predicate n args -> or $ map (\t' -> t == t') args
-  Not f'           -> containsTerm f' t
-  And f1 f2        -> containsTerm f1 t || containsTerm f2 t
-  Or f1 f2         -> containsTerm f1 t || containsTerm f2 t
-  Implies f1 f2    -> containsTerm f1 t || containsTerm f2 t
-  Forall x f'      -> containsTerm f' t
-  Exists x f'      -> containsTerm f' t
+  Not f' -> containsTerm f' t
+  And f1 f2 -> containsTerm f1 t || containsTerm f2 t
+  Or f1 f2 -> containsTerm f1 t || containsTerm f2 t
+  Implies f1 f2 -> containsTerm f1 t || containsTerm f2 t
+  Forall x f' -> containsTerm f' t
+  Exists x f' -> containsTerm f' t
 
 -- | TODO At the moment only works on propositional logic
 formulaUnify :: Formula -> Formula -> Maybe (Tuple Substitution (List Term))
 formulaUnify f1 f2 = subTerms f1 f2 >>= unify
   where
-    -- | Returns the subterms if the two formulas have the same structure.
-    subTerms :: Formula -> Formula -> Maybe (Set (List Term))
-    subTerms = case _, _ of
-      Predicate f args1, Predicate g args2
-        | f == g, Array.length args1 == Array.length args2
-          -> Just $ Set.fromFoldable
-             [List.fromFoldable args1, List.fromFoldable args2]
-      Not a, Not b -> subTerms a b
-      And a b, And c d -> (<>) <$> subTerms a c <*> subTerms b d
-      Or a b, Or c d -> (<>) <$> subTerms a c <*> subTerms b d
-      Implies a b, Implies c d -> (<>) <$> subTerms a c <*> subTerms b d
-      _, _ -> Nothing
+  -- | Returns the subterms if the two formulas have the same structure.
+  subTerms :: Formula -> Formula -> Maybe (Set (List Term))
+  subTerms = case _, _ of
+    Predicate f args1, Predicate g args2
+      | f == g, Array.length args1 == Array.length args2 ->
+        Just
+          $ Set.fromFoldable
+              [ List.fromFoldable args1, List.fromFoldable args2 ]
+    Not a, Not b -> subTerms a b
+    And a b, And c d -> (<>) <$> subTerms a c <*> subTerms b d
+    Or a b, Or c d -> (<>) <$> subTerms a c <*> subTerms b d
+    Implies a b, Implies c d -> (<>) <$> subTerms a c <*> subTerms b d
+    _, _ -> Nothing
 
 formulaUnifier :: Formula -> Formula -> Maybe Substitution
 formulaUnifier a b = (\(Tuple σ _) -> σ) <$> formulaUnify a b
--- test/Parser.purs
+++ test/Parser.purs
@@ -4,30 +4,45 @@ import Prelude
 import Data.Either (Either(..))
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (shouldEqual)
-
-import Formula (Variable(..), Term(..), Formula(..))
+import Formula (Variable(..), Term(..), Formula(..), bottomProp)
 import Parser (parseFormula)
 
 spec :: Spec Unit
-spec = describe "Formula parser" do
-  it "should work" do
-    parseFormula "¬(A ∧ B)" `shouldEqual`
-      Right (Not (And
-                  (Predicate "A" [])
-                  (Predicate "B" [])))
-  it "should handle whitespace" do
-    parseFormula " ∃ x P ( x ) → ( A ) " `shouldEqual`
-      Right (Implies
-             (Exists (Variable "x") (Predicate "P" [(Var $ Variable "x")]))
-             (Predicate "A" []))
-  it "parses constants and variables" do
-    parseFormula "∀x Le(x, c())" `shouldEqual`
-      Right (Forall (Variable "x")
-            (Predicate "Le" [Var $ Variable "x", App "c" []]))
-  it "parses the equality predicate" do
-    parseFormula "=(x, y)" `shouldEqual`
-      Right (Predicate "=" [Var $ Variable "x", Var $ Variable "y"])
-    parseFormula "x = y" `shouldEqual`
-      Right (Predicate "=" [Var $ Variable "x", Var $ Variable "y"])
-  it "should parse double negation" do
-    parseFormula "¬¬P" `shouldEqual` Right (Not (Not (Predicate "P" [])))
+spec =
+  describe "Formula parser" do
+    it "should work" do
+      parseFormula "¬(A ∧ B)"
+        `shouldEqual`
+          Right
+            ( Not
+                ( And
+                    (Predicate "A" [])
+                    (Predicate "B" [])
+                )
+            )
+    it "should handle whitespace" do
+      parseFormula " ∃ x P ( x ) → ( A ) "
+        `shouldEqual`
+          Right
+            ( Implies
+                (Exists (Variable "x") (Predicate "P" [ (Var $ Variable "x") ]))
+                (Predicate "A" [])
+            )
+    it "parses constants and variables" do
+      parseFormula "∀x Le(x, c())"
+        `shouldEqual`
+          Right
+            ( Forall (Variable "x")
+                (Predicate "Le" [ Var $ Variable "x", App "c" [] ])
+            )
+    it "parses the equality predicate" do
+      parseFormula "=(x, y)"
+        `shouldEqual`
+          Right (Predicate "=" [ Var $ Variable "x", Var $ Variable "y" ])
+      parseFormula "x = y"
+        `shouldEqual`
+          Right (Predicate "=" [ Var $ Variable "x", Var $ Variable "y" ])
+    it "should parse double negation" do
+      parseFormula "¬¬P" `shouldEqual` Right (Not (Not (Predicate "P" [])))
+    it "can parse the bottom symbol" do
+      parseFormula "⊥" `shouldEqual` Right bottomProp
