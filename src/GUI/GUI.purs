6ff335f1d748111e8f9bf10d8f47a3d1e0c0437f
tessagon <gusandthak@student.gu.se>
2021-04-14 14:12:41 +0200

    initial commit

--- src/GUI/GUI.purs
+++ src/GUI/GUI.purs
@@ -114,9 +114,240 @@ siteBody =
     manualBody = HH.text lorem
 
   shortcutModal :: Boolean -> HH.HTML _ _
-  shortcutModal isActive = mkModal "Syntax shortcuts." shortcutBody isActive
+  shortcutModal isActive = mkModal "Syntax shortcuts." shortCutBody isActive
     where
-    shortcutBody = HH.text lorem
+    shortCutBody = HH.div_
+                     [ HH.section [ HP.classes [ HH.ClassName "section"]] 
+                     [HH.text $ "This section lists various shortcut commands you can " <>
+                               "type or press with the keyboard when constructing various proofs."]
+                     , HH.section [ HP.classes [ HH.ClassName "section"]] [
+                       HH.h1 [HP.classes [ HH.ClassName "title", HH.ClassName "has-text-centered"]] [HH.text "General"]
+                     , HH.table [HP.classes [HH.ClassName "table"]]
+                         [ HH.thead_
+                             [ HH.tr_
+                                 [ HH.th_ [HH.abbr [HP.title "Commands"] [HH.text "Command"]]
+                                 , HH.th_ [HH.abbr [HP.title "Explanation of the commands"] [HH.text "Explanation"]]
+                                 , HH.th_ [HH.abbr [HP.title "Keyboard shortcuts"] [HH.text "Keyboard shortcuts"]]
+                                 ]
+                             ]
+                         , HH.tbody []
+                             [ HH.tr_
+                                 [ HH.th_ [ HH.text "Add row"]
+                                 , HH.td_ [ HH.text "Add an empty row beneath a current one."]
+                                 , HH.td_ [ HH.strong_ [ HH.text "enter"]]
+                                 ]
+                             , HH.tr_
+                                 [ HH.th_ [ HH.text "Delete row"]
+                                 , HH.td_ [ HH.text "Delete a current row. Note, the formula field in the proof panel needs to be empty to remove a row. If it is not empty, then pressing backspace until the formula is gone will delete the row."]
+                                 , HH.td_ [ HH.strong_ [ HH.text "backspace"]]
+                                 ]
+                             , HH.tr_
+                                 [ HH.th_ [ HH.text "Add Assumption box"]
+                                 , HH.td_ [ HH.text "Opens an assumption box at a specific row when inputting the shortcut in the rule field."]
+                                 , HH.td_ [ HH.strong_ [ HH.text "TODO"]]
+                                 ]
+                             , HH.tr_
+                                 [ HH.th_ [ HH.text "Add row outside of assumption box"]
+                                 , HH.td_ [ HH.text "When inside an assumption box, pressing enter will add a row in that current box. This command is used to jump outside of the box and add a row outside of it."]
+                                 , HH.td_ [ HH.strong_ [ HH.text "Alt+Enter"]]
+                                 ]
+                             , HH.tr_
+                                 [ HH.th_ [ HH.text "Add Premise"]
+                                 , HH.td_ [ HH.text "Adds a Premise in the rule field."]
+                                 , HH.td_ [ HH.strong_ [ HH.text "TODO"]]
+                                 ]
+                             ]
+                         ]
+                     ]
+                     , HH.h1 [HP.classes [ HH.ClassName "has-text-centered"]] [HH.text "Symbols"]
+                     , HH.table [HP.classes [HH.ClassName "table", HH.ClassName "is-fullwidth"]]
+                         [ HH.thead_
+                             [ HH.tr_
+                                 [ HH.th_ [HH.abbr [HP.title "Symbols defined in the logic"] [HH.text "Symbol"]]
+                                 , HH.th_ [HH.abbr [HP.title "Connectives"] [HH.text "Connective"]]
+                                 , HH.th_ [HH.abbr [HP.title "Keyboard shortcuts for inputting symbols"] [HH.text "Keyboard shortcuts"]]
+                                 ]
+                             ]
+                         , HH.tbody []
+                             [ HH.tr_
+                                 [ HH.th_ [ HH.text "∧"]
+                                 , HH.td_ [ HH.text "Conjunction"]
+                                 , HH.td_ [ HH.strong_ [ HH.text "an"]]
+                                 ]
+                             , HH.tr_
+                                 [ HH.th_ [ HH.text "∨"]
+                                 , HH.td_ [ HH.text "Disjunction"]
+                                 , HH.td_ [ HH.strong_ [ HH.text "or"]
+                                          , HH.text ", "
+                                          , HH.strong_ [ HH.text "v"]
+                                          ]
+                                 ]
+                             , HH.tr_
+                                 [ HH.th_ [ HH.text "→"]
+                                 , HH.td_ [ HH.text "Implication"]
+                                 , HH.td_ [ HH.strong_ [ HH.text "->"]
+                                          , HH.text ", "
+                                          , HH.strong_ [ HH.text "imp"]
+                                          ]
+                                 ]
+                             , HH.tr_
+                                 [ HH.th_ [ HH.text "¬"]
+                                 , HH.td_ [ HH.text "Negation"]
+                                 , HH.td_ [ HH.strong_ [ HH.text "not"]
+                                          , HH.text ", "
+                                          , HH.strong_ [ HH.text "neg"]
+                                          , HH.text ", "
+                                          , HH.strong_ [ HH.text "!"]
+                                          ]
+                                 ]
+                             , HH.tr_
+                                 [ HH.th_ [ HH.text "∀"]
+                                 , HH.td_ [ HH.text "Universal, for all"]
+                                 , HH.td_ [ HH.strong_ [ HH.text "fa"]
+                                          , HH.text ", "
+                                          , HH.strong_ [ HH.text "fo"]
+                                          ]
+                                 ]
+                             , HH.tr_
+                                 [ HH.th_ [ HH.text "∃"]
+                                 , HH.td_ [ HH.text "Existential, there exist"]
+                                 , HH.td_ [ HH.strong_ [ HH.text "ex"]
+                                          , HH.text ", "
+                                          , HH.strong_ [ HH.text "te"]
+                                          ]
+                                 ]
+                             , HH.tr_
+                                 [ HH.th_ [ HH.text "⊥"]
+                                 , HH.td_ [ HH.text "Bottom / Contradiction"]
+                                 , HH.td_ [ HH.strong_ [ HH.text "bo"]
+                                          , HH.text ", "
+                                          , HH.strong_ [ HH.text "con"]
+                                          ]
+                                 ]
+                             ]
+                         ]
+                     , HH.h1 [HP.classes [ HH.ClassName "has-text-centered"]] [HH.text "Rules"]
+                     , HH.table [HP.classes [HH.ClassName "table", HH.ClassName "is-fullwidth"]]
+                         [ HH.thead_
+                             [ HH.tr_
+                                 [ HH.th_ [HH.abbr [HP.title "Symbolic representation of the rules"] [HH.text "Symbol"]]
+                                 , HH.th_ [HH.abbr [HP.title "Rule"] [HH.text "Rule"]]
+                                 , HH.th_ [HH.abbr [HP.title "Keyboard shortcuts typed in the rule field when constructing proofs"] [HH.text "Keyboard shortcuts"]]
+                                 ]
+                             ]
+                         , HH.tbody []
+                             [ HH.tr_
+                                 [ HH.th_ [ HH.text "∧i"]
+                                 , HH.td_ [ HH.text "Conjunction introduction"]
+                                 , HH.td_ [ HH.strong_ [ HH.text "ani"]]
+                                 ]
+                             , HH.tr_
+                                 [ HH.th_ [ HH.text "∧e", HH.sub_ [ HH.text "1"]]
+                                 , HH.td_ [ HH.text "Conjunction elimination 1"]
+                                 , HH.td_ [ HH.strong_ [ HH.text "ane1"]]
+                                 ]
+                             , HH.tr_
+                                 [ HH.th_ [ HH.text "∧e", HH.sub_ [ HH.text "2"]]
+                                 , HH.td_ [ HH.text "Conjunction elimination 2"]
+                                 , HH.td_ [ HH.strong_ [ HH.text "ane2"]]
+                                 ]
+                             , 
+                             HH.tr_
+                                 [ HH.th_ [ HH.text "∨i", HH.sub_ [ HH.text "1"]]
+                                 , HH.td_ [ HH.text "Disjunction introduction 1"]
+                                 , HH.td_ [ HH.strong_ [ HH.text "ori1"]
+                                          , HH.text ", "
+                                          , HH.strong_ [ HH.text "∨i1"]]
+                                 ]
+                             , HH.tr_
+                                 [ HH.th_ [ HH.text "∨i", HH.sub_ [ HH.text "2"]]
+                                 , HH.td_ [ HH.text "Disjunction introduction 2"]
+                                 , HH.td_ [ HH.strong_ [ HH.text "ori2"]
+                                          , HH.text ", "
+                                          , HH.strong_ [ HH.text "∨i2"]]
+                                 ]
+                             , HH.tr_
+                                 [ HH.th_ [ HH.text "∨e"]
+                                 , HH.td_ [ HH.text "Disjunction elimination"]
+                                 , HH.td_ [ HH.strong_ [ HH.text "ore"]
+                                          , HH.text ", "
+                                          , HH.strong_ [ HH.text "∨e"]]
+                                 ]
+                             , HH.tr_
+                                 [ HH.th_ [ HH.text "→i"]
+                                 , HH.td_ [ HH.text "Implication introduction"]
+                                 , HH.td_ [ HH.strong_ [ HH.text "->i"]
+                                          , HH.text ", "
+                                          , HH.strong_ [ HH.text "impi"]]
+                                 ]
+                             , HH.tr_
+                                 [ HH.th_ [ HH.text "→e"]
+                                 , HH.td_ [ HH.text "Implication elimination"]
+                                 , HH.td_ [ HH.strong_ [ HH.text "->e"]
+                                          , HH.text ", "
+                                          , HH.strong_ [ HH.text "impe"]]
+                                 ]
+                             , HH.tr_
+                                 [ HH.th_ [ HH.text "¬i"]
+                                 , HH.td_ [ HH.text "Negation introduction"]
+                                 , HH.td_ [ HH.strong_ [ HH.text "note"]
+                                          , HH.text ", "
+                                          , HH.strong_ [ HH.text "nege"]
+                                          , HH.text ", "
+                                          , HH.strong_ [ HH.text"!e"]]
+                                 ]
+                             , HH.tr_
+                                 [ HH.th_ [ HH.text "¬e"]
+                                 , HH.td_ [ HH.text "Negation elimination"]
+                                 , HH.td_ [ HH.strong_ [ HH.text "noti"]
+                                          , HH.text ", "
+                                          , HH.strong_ [ HH.text "negi"]
+                                          , HH.text ", "
+                                          , HH.strong_ [ HH.text"!i"]]
+                                 ]
+                             , HH.tr_
+                                 [ HH.th_ [ HH.text "⊥e"]
+                                 , HH.td_ [ HH.text "Bottom elimination"]
+                                 , HH.td_ [ HH.strong_ [ HH.text "boe"]
+                                          , HH.text ", "
+                                          , HH.strong_ [ HH.text "cone"]]
+                                 ]
+                             , HH.tr_
+                                 [ HH.th_ [ HH.text "¬¬e"]
+                                 , HH.td_ [ HH.text "Double negation elimination"]
+                                 , HH.td_ [ HH.strong_ [ HH.text "notnote"]
+                                          , HH.text ", "
+                                          , HH.strong_ [ HH.text "negnege"]
+                                          , HH.text ", "
+                                          , HH.strong_ [ HH.text "!!e"]]
+                                 ]
+                             , HH.tr_
+                                 [ HH.th_ [ HH.text "¬¬i"]
+                                 , HH.td_ [ HH.text "Double negation introduction"]
+                                 , HH.td_ [ HH.strong_ [ HH.text "notnoti"]
+                                          , HH.text ", "
+                                          , HH.strong_ [ HH.text "negnegi"]
+                                          , HH.text ", "
+                                          , HH.strong_ [ HH.text "!!i"]]
+                                 ]
+                             , HH.tr_
+                                 [ HH.th_ [ HH.text "MT"]
+                                 , HH.td_ [ HH.text "Modus Tollens"]
+                                 , HH.td_ [ HH.strong_ [ HH.text "MT"]]
+                                 ]
+                             , HH.tr_
+                                 [ HH.th_ [ HH.text "PBC"]
+                                 , HH.td_ [ HH.text "Proof by Contraction"]
+                                 , HH.td_ [ HH.strong_ [ HH.text "TODO"]]
+                                 ]
+                             , HH.tr_
+                                 [ HH.th_ [ HH.text "LEM"]
+                                 , HH.td_ [ HH.text "Law of Excluded Middle"]
+                                 , HH.td_ [ HH.strong_ [ HH.text "TODO"]]
+                                 ]
+                             ]
+                         ]
+                     ]
 
   mkModal :: String -> (HH.HTML _ _) -> Boolean -> HH.HTML _ _
   mkModal title modalBody isActive =
