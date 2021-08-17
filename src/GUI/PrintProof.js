d8edea07af5e99aa60cd5fd2e160b5c64bb17fe3
Rantaa <gusranlo@student.gu.se>
2021-08-17 13:11:50 +0200

    span across several pages

--- src/GUI/PrintProof.js
+++ src/GUI/PrintProof.js
@@ -5,13 +5,26 @@ const html2canvas = require('html2canvas')
 
 
 exports.printProofer = () => {
-    html2canvas(document.getElementById("printable"), {scale:0.5}).then(function(canvas) {
+    html2canvas(document.getElementById("printable")).then(function(canvas) {
         document.body.appendChild(canvas);
         var imgdata = canvas.toDataURL("image/png");
+        var imgWidth = 105;
+        var pageHeight = 295;
+        var imgHeight = canvas.height * imgWidth / canvas.width;
+        var heightLeft = imgHeight;
+        var position = 5;
+
         var doc = new jspdf.jsPDF();
-        var width = doc.internal.pageSize.getWidth();    
-        var height = doc.internal.pageSize.getHeight();
-        doc.addImage(imgdata, "PNG", 5, 5);
+        doc.addImage(imgdata, "PNG", 52.5, position, imgWidth, imgHeight);
+        heightLeft -= pageHeight;
+        
+        while (heightLeft >= 0) {
+            position = heightLeft - imgHeight;
+            doc.addPage();
+            doc.addImage(imgdata, "PNG", 52, position, imgWidth, imgHeight);
+            heightLeft -= pageHeight;
+        }
+
         doc.save("proof.pdf");
     });
 }
